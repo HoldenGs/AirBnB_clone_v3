aec0fc482064463e4c9ffb32e2271ff35d82fea2
Holden Grissett <holdeng96@gmail.com>
2017-04-24 23:11:52 +0000

    add new route for object numbers by class

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -6,9 +6,22 @@
 
 from api.v1.views import app_views
 from flask import jsonify
+from models import storage
 
 
 @app_views.route('/status')
 def get_status():
     status = {"status": "OK"}
     return jsonify(status)
+
+@app_views.route('/stats')
+def get_stats():
+    stats = {
+        "amenities": storage.count('Amenity'),
+        "cities": storage.count('City'),
+        "places": storage.count('Place'),
+        "reviews": storage.count('Review'),
+        "states": storage.count('State'),
+        "users": storage.count('User')
+    }
+    return jsonify(stats)
