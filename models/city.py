79f68a7aac8eacdb30dfc8a63131a7e90c748836
Holden Grissett <holdeng96@gmail.com>
2017-04-26 04:51:15 +0000

    add city->places relationship

--- models/city.py
+++ models/city.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 from models.base_model import BaseModel, Base, Table, Column, String
 from sqlalchemy import ForeignKey
+from sqlalchemy import relationship, backref
 from os import getenv
 """
 city module
@@ -17,6 +18,8 @@ class City(BaseModel, Base):
         __tablename__ = 'cities'
         state_id = Column(String(60), ForeignKey('states.id'), nullable=False)
         name = Column(String(128), nullable=False)
+        places = relationship('Place', backref='state',
+                              cascade='all, delete, delete-orphan')
     else:
         name = ""
         state_id = ""
@@ -26,3 +29,14 @@ class City(BaseModel, Base):
         Initializes from BaseModel
         """
         super().__init__(*args, **kwargs)
+
+    if getenv('HBNB_TYPE_STORAGE', 'fs') != 'db':
+        @property
+        def places(self):
+            """
+            returns all cities in a State
+            """
+            all_places = models.storage.all("Place").values()
+            result = [place for place in all_places
+                      if place.city_id == self.id]
+            return result
