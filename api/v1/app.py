0300c29242a4f7b6041f708784a2852bdfa44c78
Holden Grissett <holdeng96@gmail.com>
2017-04-26 06:34:33 +0000

    try some new things for CORS

--- api/v1/app.py
+++ api/v1/app.py
@@ -6,13 +6,14 @@ Flask app
 
 from api.v1.views import app_views
 from flask import Flask, jsonify
-from flask_cors import CORS
+from flask_cors import CORS, cross_origin
 from models import storage
 from os import getenv
 
 app = Flask(__name__)
-app.register_blueprint(app_views)
 CORS(app, resources={r"/*": {"origins": "0.0.0.0"}})
+app.register_blueprint(app_views)
+
 
 
 @app.errorhandler(404)
